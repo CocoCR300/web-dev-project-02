02364498a78afb5b602924e604b041c3c06cb731
CocoCR300 <55771921+CocoCR300@users.noreply.github.com>
2025-06-04 20:24:00 -0600

    add shadcn library

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
 import './App.css'
+import { Home, Search, Settings } from 'lucide-react'
+import { ThemeProvider } from './theme-provider';
+
+const tabItems = [
+	{
+		content: "Hello, world!",
+		key: "home",
+		title: "Home",
+		url: "#",
+		icon: <Home/>,
+	},
+	{
+		content: "Hello, search!",
+		key: "search",
+		title: "Search",
+		url: "#",
+		icon: <Search/>,
+	},
+	{
+		content: "Hello, settings!",
+		key: "settings",
+		title: "Settings",
+		url: "#",
+		icon: <Settings/>,
+	},
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<ThemeProvider>
+			<Tabs defaultValue="account" style={{
+				display: "flex",
+				flexFlow: "column",
+				height: "100%",
+				padding: "1em",
+				width: "100%"
+			}}>
+				<div style={{ flex: 1, marginBottom: "0.5em" }}>
+					{
+						tabItems.map(item => (
+							<TabsContent value={item.key}>
+								{ item.content }
+							</TabsContent>
+						))
+					}
+				</div>
+				<TabsList style={{ columnGap: "0.5em", display: "flex", justifyContent: "center" }}>
+					{
+						tabItems.map(item => (
+							<TabsTrigger value={item.key}>
+								{ item.icon }
+								{ item.title }
+							</TabsTrigger>
+						))
+					}
+				</TabsList>
+			</Tabs>
+		</ThemeProvider>
+	)
 }
 
-export default App
+export default App;
